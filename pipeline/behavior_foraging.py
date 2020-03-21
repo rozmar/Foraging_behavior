37e0c3d7b893b31e10f06afd061b348c59175ba9
tienn <u931650@gmail.com>
2020-03-21 15:54:33 -0400

    test

--- pipeline/behavior_foraging.py
+++ pipeline/behavior_foraging.py
@@ -5,7 +5,7 @@ from pipeline.pipeline_tools import get_schema_name
 schema = dj.schema(get_schema_name('behavior_foraging'),locals())
 import numpy as np
 import pandas as pd
-#%
+#%%
 @schema
 class TrialReactionTime(dj.Computed):
     definition = """
